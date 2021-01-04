e50a004f2525e71960789aa53cee6f136d669412
HaopengSun <sunhaopeng@gmail.com>
2021-01-04 22:09:30 +0000

    usage of console.assert

--- head.js
+++ head.js
@@ -8,6 +8,13 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const head = function(arr){
+  if (arr.length === 0){
+    return [];
+  } else {
+      return arr[0];
+  }
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
