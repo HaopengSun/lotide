f4532f7b0a1ffd33221b534be417f5626458a1f1
HaopengSun <sunhaopeng@gmail.com>
2021-01-05 18:35:07 +0000

    equal array function

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+    let correctMsg = 'Assertion Passed: ' + actual + '===' + expected;
+    let errorMsg = 'Assertion Failed: ' + actual + '!==' + expected;
+    if (actual === expected) {
+        console.log(correctMsg);
+    } else {
+        console.log(errorMsg);
+    }
+};
+
+const eqArrays = function(arr1, arr2){
+    if (arr1.length === arr2.length){
+        for (let i = 0; i < arr1.length; i++){
+            if (arr1[i] === arr2[i]){
+                continue;
+            } else {
+                return false;
+            }
+        }
+    } else {
+        return false;
+    }
+    return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
