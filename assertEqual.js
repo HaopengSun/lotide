f8628c5b3e3a1fe42b4110f7271f8894b1157b7f
HaopengSun <sunhaopeng@gmail.com>
2021-01-05 00:57:27 +0000

    not have to use console.assert()

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,12 @@
 const assertEqual = function(actual, expected) {
   // let correctMsg = `Assertion Passed: ${actual} === ${expected}`;
   let correctMsg = 'Assertion Passed: ' + actual + '===' + expected;
-  let errorMsg = actual + '!==' + expected;
-  console.assert(actual === expected, errorMsg);
+  let errorMsg = 'Assertion Failed: ' + actual + '!==' + expected;
+  // console.assert(actual === expected, errorMsg);
   if (actual === expected) {
     console.log(correctMsg);
+  } else {
+    console.log(errorMsg);
   }
 };
 
