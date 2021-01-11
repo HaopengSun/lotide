26387b65da42d295ef93b0463e8bf8bfeb5ac092
HaopengSun <sunhaopeng@gmail.com>
2021-01-11 18:23:37 +0000

    add expect

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,5 @@
 // const assertArraysEqual = require('../assertArraysEqual');
+const { expect } = require('chai');
 const eqArrays = require('../eqArrays');
 const middle = require('../middle');
 // const { assert } = require('chai') 
@@ -11,19 +12,22 @@ const assert = require('chai').assert;
 // assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
 
 describe('test middle function', function(){
-  it('should return true for [1]', () => {
-    assert.isTrue(eqArrays(middle([1]), []));
+  it('should return emply for [1]', () => {
+    expect(middle([1])).to.be.empty;
+    // assert.isTrue(eqArrays(middle([1]), []));
   });
-  it('should return true for [1, 2]', () => {
+  it('should return empty for [1, 2]', () => {
     assert.isTrue(eqArrays(middle([1, 2]), []));
   });
-  it('should return true for [1, 2, 3, 4, 5]', () => {
-    assert.isTrue(eqArrays(middle([1, 2, 3, 4, 5]), [3]));
+  it('should return [3] for [1, 2, 3, 4, 5]', () => {
+    const result = middle([1, 2, 3, 4, 5]);
+    // assert.isTrue(eqArrays(result, [3]));
+    expect(result).to.be.deep.equal([3]);
   });
-  it('should return true for [1, 2, 3, 4]', () => {
+  it('should return [2, 3] for [1, 2, 3, 4]', () => {
     assert.isTrue(eqArrays(middle([1, 2, 3, 4]), [2, 3]));
   });
-  it('should return true [1, 2, 3, 4, 5, 6]', () => {
+  it('should return [3, 4] for [1, 2, 3, 4, 5, 6]', () => {
     assert.isTrue(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
   });
 });
