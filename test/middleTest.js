8d6ef12a1d1487305b748bb8bbb6a81c7502d2bc
HaopengSun <sunhaopeng@gmail.com>
2021-01-11 13:50:20 +0000

    change middletest function using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,28 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
 const eqArrays = require('../eqArrays');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(eqArrays(middle([1]), []), true);
-assertArraysEqual(eqArrays(middle([1, 2]), []), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
+// assertArraysEqual(eqArrays(middle([1]), []), true);
+// assertArraysEqual(eqArrays(middle([1, 2]), []), true);
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
+// assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
+
+describe('test middle function', function(){
+  it('should return true for [1]', () => {
+    assert.isTrue(eqArrays(middle([1]), []));
+  });
+  it('should return true for [1, 2]', () => {
+    assert.isTrue(eqArrays(middle([1, 2]), []));
+  });
+  it('should return true for [1, 2, 3, 4, 5]', () => {
+    assert.isTrue(eqArrays(middle([1, 2, 3, 4, 5]), [3]));
+  });
+  it('should return true for [1, 2, 3, 4]', () => {
+    assert.isTrue(eqArrays(middle([1, 2, 3, 4]), [2, 3]));
+  });
+  it('should return true [1, 2, 3, 4, 5, 6]', () => {
+    assert.isTrue(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+  });
+});
