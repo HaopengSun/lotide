0630e87e78c2a85d8f568cf6b35e485658ac0a7b
HaopengSun <sunhaopeng@gmail.com>
2021-01-11 14:00:39 +0000

    lodash function requires object exports

---
+++ lodash.js
@@ -0,0 +1,12 @@
+// npm i --save lodash
+// lodash export an object
+const _ = require('lodash');
+
+console.log(_.head([1, 2, 3])); // => 1
+console.log(_.tail([1, 2, 3])); // => [2, 3]
+
+// const head = require('./head');
+// const tail = require('./tail');
+
+// head([1, 2, 3]); // => 1
+// tail([1, 2, 3]); // => [2, 3]
