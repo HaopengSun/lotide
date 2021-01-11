be6f6ae80ae2ffbdea70b99aa2dfd45d28f0e66d
HaopengSun <sunhaopeng@gmail.com>
2021-01-11 03:20:02 +0000

    add export and require module

--- head.js
+++ head.js
@@ -1,20 +1,19 @@
-const assertEqual = function(actual, expected) {
-  // let correctMsg = `Assertion Passed: ${actual} === ${expected}`;
-  let correctMsg = 'Assertion Passed: ' + actual + '===' + expected;
-  let errorMsg = actual + '!==' + expected;
-  console.assert(actual === expected, errorMsg);
-  if (actual === expected) {
-    console.log(correctMsg);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   // let correctMsg = `Assertion Passed: ${actual} === ${expected}`;
+//   let correctMsg = 'Assertion Passed: ' + actual + '===' + expected;
+//   let errorMsg = actual + '!==' + expected;
+//   console.assert(actual === expected, errorMsg);
+//   if (actual === expected) {
+//     console.log(correctMsg);
+//   }
+// };
 
 const head = function(arr){
   if (arr.length === 0){
     return [];
   } else {
-      return arr[0];
+    return arr[0];
   }
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
